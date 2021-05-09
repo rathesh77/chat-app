d282517dd41ae66cef00071bd4022f01d676138a
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-05-09 22:56:05 +0200

    move axios.js to src folder

--- backend/repositories/SocketRepository.ts
+++ backend/repositories/SocketRepository.ts
@@ -1,8 +1,10 @@
 import { MessageI } from "../entities/Message";
-import io from 'socket.io'
+import io, { Socket } from 'socket.io'
+import { ChannelI } from '../entities/Channel'
+
 export interface SocketRepository {
     onConnection(socket: io.Socket): void;
     broadcastMessage(data: MessageI): void; // emit message to every other users
-    noticeThatAUserIsTyping(clientId: string, fullname: string): void; // user is typing ....
-    noticeThatAUserIsNotTypingAnymore(clientId: string): void;
+    noticeThatAUserIsTyping(client: Socket, fullname: string, channel: ChannelI): void; // user is typing ....
+    noticeThatAUserIsNotTypingAnymore(client: Socket, channel: ChannelI): void;
 }
