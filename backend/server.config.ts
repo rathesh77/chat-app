aaf03b8a341023f74b36508bfe165103ff23c977
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-05-09 18:17:18 +0200

    remove useless stripe properties

--- backend/server.config.ts
+++ backend/server.config.ts
@@ -5,8 +5,4 @@ export default {
         database: 'bdd',
         password: 'superaccount'
     },
-    stripe:{
-        public_key:'pk_test_51HvkwYLjW8CJv9Axomn1cYBMLuvJ6hVBb002isuzWJTJ7beBM347sA1AZhVi4skpiiHmrl3wL1OPQ2J0InSSSF01004lSOoVFE',
-        private_key:'sk_test_51HvkwYLjW8CJv9AxRtaXlI4lnFhzqdzt4l28eFeiYvaOAxj2RWDJroxxVSRSuRUYlQIg8rQbPIJwnW3ELjZuh6Os00MaOtuZGb'
-    }
 }
