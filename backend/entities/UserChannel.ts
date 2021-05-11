935918058c99c056db6e3b08aeb4e3a0d61b356e
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-05-12 00:06:43 +0200

    fix fetching messages request

--- backend/entities/UserChannel.ts
+++ backend/entities/UserChannel.ts
@@ -37,13 +37,29 @@ class UserChannel {
                    ON us.id_channel = c.id
                    and us.id_user = $1
                 `*/
-                `SELECT c.id as channel_id, c.name, c.author as channel_author,u.name as  message_author_name , m.content, m.id_client as message_author
-
-                    FROM ${UserChannel.tableName} as uc 
-                    LEFT JOIN ${Channel.tableName} as c ON uc.id_channel = c.id
-                    LEFT JOIN ${Message.tableName} as m on c.id = m.id_channel 
-                    LEFT JOIN ${User.tableName} as u on m.id_client = u.id
-                    and uc.id_user = $1;
+                `SELECT 
+                subquery.channel_id as channel_id, 
+                subquery.channel_name, 
+                subquery.channel_author  as channel_author_id, 
+                u.name as message_author_name, 
+                m.content, 
+                m.id_client as message_author_id,
+                m.creation_date
+              FROM 
+                (
+                  select 
+                    c2.id as channel_id, 
+                    c2.name as channel_name, 
+                    c2.author as channel_author 
+                  from 
+                    ${User.tableName} as u2 
+                    INNER JOIN ${UserChannel.tableName} as uc2 on uc2.id_user = u2.id 
+                    INNER JOIN ${Channel.tableName} as c2 on c2.id = uc2.id_channel 
+                    and u2.id = $1
+                ) as subquery 
+                LEFT OUTER JOIN ${Message.tableName} as m on subquery.channel_id = m.id_channel 
+                LEFT OUTER JOIN ${User.tableName} as u on m.id_client = u.id 
+                ORDER BY m.creation_date asc
                    `,
             values: [
                 clientId
