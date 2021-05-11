23b505824772bbd5398809bbdfed24d9852ff2a8
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-05-12 01:36:12 +0200

    refactor socket event and remove useless logs

--- backend/server.ts
+++ backend/server.ts
@@ -41,7 +41,7 @@ app.use(sessionMiddleware)
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:8080' // si votre port est différent, changez cette valeur !
+    origin: 'http://localhost:8080'
 }))
 app.use(express.urlencoded())
 app.use(express.json());
@@ -54,64 +54,52 @@ const socketController: SocketController = new SocketController(new inMemorySock
 socket.on('connection', async (client: io.Socket) => {
     socketController.onConnection(client)
 
-    console.log(client.handshake.session);
-
     if (client.handshake.session?.user) {
         let currentUser = client.handshake.session.user
         // on recupere la liste des channels du client
         let userChannels = await UserChannel.findByUserId(currentUser.id)
-        // le client rejoint tous les channels où il est
+        // le client rejoint tous les channels où il etait precedemment
         let previousRoom = null
         for (let channel of userChannels) {
-            if ( previousRoom != `${channel.channel_name}:${channel.channel_author_id}`){
+            if (previousRoom != `${channel.channel_name}:${channel.channel_author_id}`) {
                 client.join(`${channel.channel_name}:${channel.channel_author_id}`)
                 previousRoom = `${channel.channel_name}:${channel.channel_author_id}`
             }
 
         }
-        
+
         client.emit('channelsList', userChannels)
     }
     client.on('message', async (data: MessageI) => {
-        //client.handshake.session!.user = { user: "rathesh" }
-        //console.log(client.handshake.session?.user);
-
-        //console.log(client.handshake.session?.user);
-        //console.log(client.request.headers.cookie);
-        //client.handshake.session?.save()
 
         let channel = data.channel
         let message = data.content
-        console.log(channel);
-        
+
         // recuperer le channel où le msg est sur le point d'être envoyé
-        let currentChannel = await UserChannel.findByUserIdAndChannelId(client.handshake.session?.user?.id, channel.name.substring(channel.name.indexOf(':') + 1))
+        let currentChannelId = await Channel.findByNameAndAuthor(channel.name.substring(0, channel.name.indexOf(':')), channel.author);
+        let currentChannel = await UserChannel.findByUserIdAndChannelId(client.handshake.session?.user?.id, currentChannelId.id)
         // tester si on est bien dans le channel 
         if (!currentChannel) {
             console.log('vous n"etes pas dans ce channel')
             return
         }
-        let currentChannelId= await Channel.findByNameAndAuthor(channel.name.substring(0, channel.name.indexOf(':')), channel.author);
-        
         channel.id = currentChannelId.id
-        //socketController.broadcastMessage(data)
         await Message.create(client.handshake.session?.user?.id, channel, message)
-        console.log(data)
         socket.to(`${channel.name}`).emit('messageReceived', data)
     })
     client.on('createChannel', async (channelName: string) => {
-        //socketController.noticeThatAUserIsTyping(client, client.handshake.session?.user?.name, channel)
         const userId = client.handshake.session?.user?.id
         let channelToCreate = await Channel.create(channelName, userId)
-       let response =  await UserChannel.create(channelToCreate.id, userId)
+        await UserChannel.create(channelToCreate.id, userId)
         client.join(`${channelName}:${userId}`)
 
     })
-    client.on('signal', async (channel: ChannelI) => {
-        socketController.noticeThatAUserIsTyping(client, client.handshake.session?.user?.name, channel)
+    client.on('userIsTyping', async (channelName: string) => {
+        client.to(channelName).emit('userIsTyping', { channelName, user: client.handshake.session?.user })
     })
-    client.on('userIsNotTypingAnymore', async (channel: ChannelI) => {
-        socketController.noticeThatAUserIsNotTypingAnymore(client, channel)
+    client.on('userIsNotTypingAnymore', async (channelName: string) => {
+        client.to(channelName).emit('userIsNotTypingAnymore', { channelName, user: client.handshake.session?.user })
+
     })
 
     client.on('disconnect', () => {
@@ -119,19 +107,7 @@ socket.on('connection', async (client: io.Socket) => {
     })
 })
 
-/*
-app.get("/", function (req, res) {
-      if (!req.session.user)
-          req.session.user = { 'user': 'rathesh' }
-      else
-          console.log("session already exists");
-  
-    //console.log(req.session);
-    //res.send('hello')
-});*/
-
 app.post('/register', mustNotBeAuthenticated, async function (req, res) {
-    console.log(req.body);
 
     if (!req.body || !req.body.email || !req.body.password || !req.body.name) {
         res.status(403)
@@ -184,7 +160,6 @@ app.get('/logout', hasToBeAuthenticated, function (req, res) {
 })
 
 app.post('/channel', hasToBeAuthenticated, async function (req, res) {
-    console.log(req.session);
 
     let channelToCreate = await Channel.create(req.body.name, req.session.user?.id)
     await UserChannel.create(channelToCreate.id, req.session.user?.id)
@@ -208,15 +183,6 @@ app.delete('/channel', async function (req, res) {
     }
     await Channel.deleteById(channelToDelete.id)
     res.send('channel deleted')
-})
-app.get('/channels', hasToBeAuthenticated, async function (req, res) {
-
-    let userChannels = await UserChannel.findByUserId(req.session?.user?.id)
-    for (let channel of userChannels) {
-        console.log(channel);
-
-    }
-    res.send("channels")
 })
 
 app.get('/me', hasToBeAuthenticated, function (req, res) {
