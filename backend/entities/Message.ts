6c356e77f6e5e48a02441edeb4efc75c2478d7d2
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-05-12 00:06:15 +0200

    add timestamp for messages

--- backend/entities/Message.ts
+++ backend/entities/Message.ts
@@ -21,18 +21,19 @@ export class Message {
                 id SERIAL PRIMARY KEY,
                 content TEXT,
                 id_channel INTEGER REFERENCES ${Channel.tableName}(id),
-                id_client INTEGER REFERENCES ${User.tableName}(id)
+                id_client INTEGER REFERENCES ${User.tableName}(id),
+                creation_date TIMESTAMPTZ
             )
         `
     }
     static async create(userId: string, channel: ChannelI, message: string) {
         const result = await PostgresStore.pgPool.query({
             text: `INSERT INTO ${Message.tableName} 
-                    (content,id_channel,id_client) values($1,$2,$3)
+                    (content,id_channel,id_client, creation_date) values($1,$2,$3,$4)
                     RETURNING *
                    `,
             values: [
-                message, channel.id, userId
+                message, channel.id, userId, new Date()
             ]
         })
         return result.rows[0]
