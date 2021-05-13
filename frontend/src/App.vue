44df6cc0728f679d73ac2dac0def54161bb372b3
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-05-13 03:52:42 +0200

    add tailwind import

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="app">
+  <div id="app" class="dark">
     <router-view />
   </div>
+  
 </template>
 
 <script>
@@ -23,9 +24,48 @@ export default {
 };
 </script>
 
-<style scoped>
-#app {
-  font-family: Arial, Helvetica, sans-serif;
+<style>
 
+:root {
+  font-size: 18px;
+}
+#app {
+  font-family: Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+  height: 100%;
+}
+
+.wrapper {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  padding: 7px  !important;
+  border-radius: 7px;
+}
+
+.dark {
+  background-color: #23272A;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
